6a1fd2d8e4b64d9e9b9914f115f83199281ed56e
music5tand <ggamit139@gmail.com>
2019-04-28 01:57:00 +0900

    Guess i'm little bit tired.

---
+++ quit_exit_shell.c
@@ -0,0 +1,22 @@
+/* Editor               : MUSIC5TAND
+   LATEST MODIFIED DATE : 2019-04-28 */
+
+#include "lib/m5shell.h"
+
+void let_me_out(void){
+    exit(1);
+}
+
+void exit_shell(void){
+ 	char *input = (char*)malloc(COM_MAX_LEN*sizeof(char));
+    int i = 1;
+    while(i){
+        printf(";p nope!\n");
+        fflush(stdout);
+        fgets(input, COM_MAX_LEN, stdin);
+        if(strcmp(input,"Let me out\n")|strcmp(input,"let me out\n")){
+            i = 0;
+        }
+    }
+    exit(1);
+}
