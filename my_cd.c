0efee3e3ccc7ba50f0ea359995a8c545ae203b4e
music5tand <ggamit139@gmail.com>
2019-04-28 14:33:15 +0900

    add command cd which needs to be improved later.

---
+++ my_cd.c
@@ -0,0 +1,23 @@
+/* Editor               : MUSIC5TAND
+   LATEST MODIFIED DATE : 2019-04-28 
+   I want some macarons. */
+
+#include "lib/m5shell.h"
+
+int run_cd(char *args[]){
+    if(args[1] != NULL){
+        if(!strcmp(args[1],"..")){                          /* command line : cd .. */
+            chdir("..");
+        }else if(!strcmp(args[1],"/")){                     /* command line : cd / */
+            chdir(getenv("HOME"));
+        }else{
+            if(chdir(args[1]) == -1){                       /* command line : cd (directory dose not exist.) */
+                perror("m5sh ");
+            }
+        }
+    }else{
+        /* command line : cd */
+        chdir(getenv("HOME"));
+    }
+   return 0;
+}
